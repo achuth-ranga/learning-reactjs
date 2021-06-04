1de404427ece7cd939806a4215a67b16e41a0bad
Ranga <cranga@LAMU02F556TMD6R.uhc.com>
2021-06-04 11:58:50 +0530

    tets

--- 05-componentsprops/src/App.js
+++ 05-componentsprops/src/App.js
@@ -3,7 +3,7 @@ import Message from './Message'
 import Summary from './Summary'
 import ReactDom from 'react-dom'
 
-let names = ["Achuth", "Chenna", "Ranga"];
+let names = ["Achuth", "Chenna", "Ranga", "TestUser"];
 
 let reverseNames = () => {
   names.reverse();
